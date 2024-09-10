47bf5c40ba4ae216473e5551cb56c0dac1c3153f
Vladimir K <iceq3004@yandex.ru>
2024-09-10 08:27:59 +0300

    Update task2_NIST.py

--- task2_NIST.py
+++ task2_NIST.py
@@ -2,10 +2,9 @@ import requests
 import scipy.special as sp
 
 
-def get_random_numbers(count):
-    url = f"https://qrng.anu.edu.au/API/jsonI.php?length={count}&type=uint16"
+def get_random_numbers():
+    url = f"https://qrng.anu.edu.au/API/jsonI.php?length={7}&type=uint16"
     response = requests.get(url)
-
     if response.status_code == 200:
         data = response.json()
         if data['success']:
@@ -13,11 +12,11 @@ def get_random_numbers(count):
             # Преобразуем каждое число в 16-битную строку и соединяем в одну строку
             bit_string = ''.join(format(num, '016b') for num in random_numbers)
             bit_string_length = len(bit_string)
-            return bit_string, bit_string_length  # Возвращаем строку битов
+            return random_numbers, bit_string, bit_string_length  # Возвращаем строку битов
         else:
-            return "Error: Could not retrieve data"
+            return None, None, None
     else:
-        return f"Error: HTTP {response.status_code}"
+        return f"Error: HTTP {response.status_code}"""
 
 
 def frequency_test(bit_string, bit_string_length):
@@ -27,9 +26,16 @@ def frequency_test(bit_string, bit_string_length):
     observedValue = abs(nth_PatrialSum) / (numberOfBits) ** 0.5
     p_Value = sp.erfc(observedValue / 2 ** 0.5)
     frequency_test_conclusion = (p_Value > 0.01)
-    return frequency_test_conclusion
+    if frequency_test_conclusion:
+        return f"Последовательность чисел является случайной, статус прохождения теста: {frequency_test_conclusion}"
+    if not frequency_test_conclusion:
+        return f"Последовательность чисел не  является случайной, статус прохождения теста: {frequency_test_conclusion}"
 
 
-# Получаем 10 случайных чисел и их битовое представление
-bit_string = get_random_numbers(7)
-print(bit_string)
+random_numbers, bit_string, bit_string_length = get_random_numbers()
+print(f'Список полученных от сервера чисел {random_numbers}')
+print(f'Битовое представления последовательности чисел {bit_string}')
+if bit_string is not None and bit_string_length is not None:
+    print(frequency_test(bit_string, bit_string_length))
+else:
+    print("Error: Could not perform frequency test due to invalid random number data")
