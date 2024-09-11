846bf7f0632b8da77a7c3584f2d1fbae29ac0d68
Vladimir K <iceq3004@yandex.ru>
2024-09-11 21:16:40 +0300

    Update task2_NIST.py

--- task2_NIST.py
+++ task2_NIST.py
@@ -49,6 +49,23 @@ def frequency_test_within_a_Block_2(bit_string):
         return f"Последовательность чисел не  является случайной, статус прохождения теста frequency_test_within_a_Block_2: {frequency_test_conclusion}"
 
 
+def run_test_3(bit_string, bit_string_length):
+    numberOf1 = 0
+    for i in bit_string:
+        if int(i) == 1:
+            numberOf1 += 1
+    proportionOf1 = numberOf1 / bit_string_length
+    tau = 2 / bit_string_length ** 0.5
+    observedNumber = 1
+    for i in range(1, len(bit_string_length)):
+        if bit_string[i] != bit_string[i - 1]:
+            observedNumber += 1
+    p_Value = sp.erfc(abs(observedNimber - 2 * bit_string_length * proportionOf1 * (1 - proportionOf1)) / (2 * (2 * bit_string_length) ** 0.5 * proportionOf1 * ( 1 - proportionOf1)))
+    frequency_test_conclusion = (p_Value > 0.01)
+    if frequency_test_conclusion:
+        return f"Последовательность чисел является случайной, статус прохождения теста run_test_3: {frequency_test_conclusion}"
+    if not frequency_test_conclusion:
+        return f"Последовательность чисел не  является случайной, статус прохождения теста run_test_3: {frequency_test_conclusion}"
 
 random_numbers, bit_string, bit_string_length = get_random_numbers()
 print(f'Список полученных от сервера чисел {random_numbers}')
