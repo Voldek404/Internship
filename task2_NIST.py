116313e867186d0926655e58d0b2222dee295422
Vladimir K <iceq3004@yandex.ru>
2024-09-11 21:31:17 +0300

    Update task2_NIST.py

--- task2_NIST.py
+++ task2_NIST.py
@@ -50,17 +50,14 @@ def frequency_test_within_a_Block_2(bit_string):
 
 
 def run_test_3(bit_string, bit_string_length):
-    numberOf1 = 0
-    for i in bit_string:
-        if int(i) == 1:
-            numberOf1 += 1
+    numberOf1 = bit_string.count('1')
     proportionOf1 = numberOf1 / bit_string_length
     tau = 2 / bit_string_length ** 0.5
     observedNumber = 1
-    for i in range(1, len(bit_string_length)):
+    for i in range(1, bit_string_length):
         if bit_string[i] != bit_string[i - 1]:
             observedNumber += 1
-    p_Value = sp.erfc(abs(observedNimber - 2 * bit_string_length * proportionOf1 * (1 - proportionOf1)) / (2 * (2 * bit_string_length) ** 0.5 * proportionOf1 * ( 1 - proportionOf1)))
+    p_Value = sp.erfc(abs(observedNumber - 2 * bit_string_length * proportionOf1 * (1 - proportionOf1)) / (2 * (2 * bit_string_length) ** 0.5 * proportionOf1 * ( 1 - proportionOf1)))
     frequency_test_conclusion = (p_Value > 0.01)
     if frequency_test_conclusion:
         return f"Последовательность чисел является случайной, статус прохождения теста run_test_3: {frequency_test_conclusion}"
@@ -70,8 +67,10 @@ def run_test_3(bit_string, bit_string_length):
 random_numbers, bit_string, bit_string_length = get_random_numbers()
 print(f'Список полученных от сервера чисел {random_numbers}')
 print(f'Битовое представления последовательности чисел {bit_string}')
+print(f'Длина строки {bit_string_length}')
 if bit_string is not None and bit_string_length is not None:
     print(frequency_test_1(bit_string, bit_string_length))
 else:
     print("Error: Could not perform frequency test due to invalid random number data")
 print(frequency_test_within_a_Block_2(bit_string))
+print(run_test_3(bit_string, bit_string_length))
